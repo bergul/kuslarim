215376bf9af7a13e64ba14c70134a9c356d017a5
boraergul <bora.ergul@live.com>
2024-12-31 19:52:59 +0300

    son

--- screens/ManageCourse.tsx
+++ screens/ManageCourse.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, View } from 'react-native'
+import { Pressable, StyleSheet, View, Text } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { Ionicons } from '@expo/vector-icons';
 
@@ -11,14 +11,8 @@ export default function ManageCourse({ route, navigation }) {
     useLayoutEffect(() => {
         navigation.setOptions({
             title: isEdit ? 'Dersi Düzenle' : 'Ders Ekle',
-            headerRight: () => (
-                <Ionicons
-                    name="trash"
-                    size={24}
-                    color="black"
-                    onPress={deleteCourse}
-                />
-            ),
+
+
         }, [isEdit, navigation]);
     });
 
@@ -28,6 +22,21 @@ export default function ManageCourse({ route, navigation }) {
 
     return (
         <View>
+            <View>
+                <Pressable onPress={() => navigation.goBack()}>
+                    <View style={{ padding: 10, backgroundColor: 'red', minWidth: 100, alignItems: 'center', borderRadius: 10 }}>
+                        <Text style={{ color: 'white' }}>İptal Et</Text>
+                    </View>
+                </Pressable>
+                <Pressable>
+                    <View style={{ padding: 10, backgroundColor: 'green', minWidth: 100, alignItems: 'center', borderRadius: 10 }}>
+                        <Text>
+                            {isEdit ? 'Dersi Güncelle' : 'Ders Ekle'}
+                        </Text>
+                    </View>
+                </Pressable>
+            </View>
+
             {isEdit && (
                 <View>
                     <Ionicons name="trash" size={24} color="black" onPress={deleteCourse} />
